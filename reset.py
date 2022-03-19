569192cfcc3d8044c9cff71eb35cac50ec180265
pasta <88070647+pastahub@users.noreply.github.com>
2022-03-18 23:46:18 -0400

    Update reset.py

--- reset.py
+++ reset.py
@@ -1,6 +1,12 @@
-import json, os, keyboard, shutil
+import json, os, sys, keyboard, shutil
 
-def reset(saves):
+def reset(saves, render_distance):
+	if not mc_open():
+		return
+	for i in range(32):
+		keyboard.press_and_release('shift+f3+f')
+	for i in range(render_distance - 2):
+		keyboard.press_and_release('f3+f')
 	keyboard.press_and_release('escape, shift+tab, enter')
 	files = os.listdir(saves)
 	files_filtered = list(filter(lambda file: os.path.isdir(os.path.join(saves, file)) and not file.startswith('_'), files))
@@ -19,6 +25,21 @@ def reset(saves):
 		except:
 			print("Unable to move " + file)
 
+def mc_open():
+	if sys.platform == 'win32':
+		import pygetwindow as gw
+		return "Minecraft" in gw.getActiveWindow().title
+	elif sys.platform == 'darwin':
+		# can't be bothered at the moment
+		return True
+	elif sys.platform == 'linux':
+		# don't know if this works and can't test it
+		import Xlib
+		disp = Xlib.display.Display()
+		window = disp.get_input_focus().focus
+
+		return "Minecraft" in window.get_wm_name()
+
 if __name__ == "__main__":
 	settings_file = open('settings.json')
 
@@ -33,5 +54,7 @@ if __name__ == "__main__":
 		os.makedirs(old)
 		print('_oldWorlds folder created')
 
-	keyboard.add_hotkey(settings['reset_hotkey'], lambda: reset(saves))
+	render_distance = settings['render_distance']
+
+	keyboard.add_hotkey(settings['reset_hotkey'], lambda: reset(saves, render_distance))
 	keyboard.wait(settings['exit_hotkey'])
